ff3e71ec84c4696d56098e9d60d5a4c12e4d7e6b
github-action-benchmark <github@users.noreply.github.com>
2026-02-19 20:25:00 +0000

    add Rust Template Benchmark (cargo) benchmark result for cfe2ce937d70dab2890c59da347a3cad60fd8774

---
+++ dev/bench/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1771532700242,
+  "repoUrl": "https://github.com/WiseMrMusa/rust-template",
+  "entries": {
+    "Rust Template Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "wisemrmusa@gmail.com",
+            "name": "Musa AbdulKareem",
+            "username": "WiseMrMusa"
+          },
+          "committer": {
+            "email": "wisemrmusa@gmail.com",
+            "name": "Musa AbdulKareem",
+            "username": "WiseMrMusa"
+          },
+          "distinct": true,
+          "id": "cfe2ce937d70dab2890c59da347a3cad60fd8774",
+          "message": "revert autopush to true",
+          "timestamp": "2026-02-19T21:23:18+01:00",
+          "tree_id": "bc9ae4a21c2d781a97a4a11e4f029916a66dd3b4",
+          "url": "https://github.com/WiseMrMusa/rust-template/commit/cfe2ce937d70dab2890c59da347a3cad60fd8774"
+        },
+        "date": 1771532699978,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "add_small_integers valid",
+            "value": 1,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "add_small_integers bound check",
+            "value": 1,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sub_small_integers valid",
+            "value": 1,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sub_small_integers bound check",
+            "value": 1,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
